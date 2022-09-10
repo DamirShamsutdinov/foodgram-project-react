5e5631197e7a95e632a3cc9b01a606ebb87af354
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-09-11 02:05:16 +0300

    Сериализатор для Юзера вынес отдельно

---
+++ backend/users/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+
+
+from recipes.models import Recipes
+from .models import CustomUser, Follow
+
+
+class ListDetailUserSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Юзер GET запрос."""
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "is_subscribed"
+        )
+
+    def get_is_subscribed(self, obj):
+        """Проверка на наличие подписок"""
+        user = self.context.get('request').user
+        if user is None or user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=obj.id).exists()
+
+
+class SupportRecipesSerializer(serializers.ModelSerializer):
+    """Сериализатор рецепта для добавления в Избранное"""
+
+    class Meta:
+        model = Recipes
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    """Сериализатор Подписок"""
+    recipes = serializers.SerializerMethodField(read_only=True)
+    recipes_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "email",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "is_subscribed",
+            "recipes",
+            "recipes_count"
+        )
+
+    def get_recipes(self, obj):
+        """Рецепты автора"""
+        recipes = obj.recipes.all()
+        return SupportRecipesSerializer(recipes, many=True).data
+
+    @staticmethod
+    def get_recipes_count(obj):
+        """Кол-во рецептов"""
+        return obj.recipes.count()
