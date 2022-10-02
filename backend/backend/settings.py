c43bfb61eae56da91e26756bcff3cea113c757ec
DamirShamsutdinov <d4mirshd@yandex.ru>
2022-10-02 14:06:05 +0600

    внес адрес ВМ в файл nginx.conf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
         "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="Industrial"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="localhost"),
         "PORT": os.getenv("DB_PORT", default="5432"),
     }
