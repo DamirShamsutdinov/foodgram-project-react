b33f56a6669f0b005b552c31146618538baa2ce7
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-10-14 14:21:29 +0600

    воркфло, тест

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,13 +4,13 @@ from rest_framework import permissions
 class IsUserSuperuserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS or
-                request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS or
-                obj.author == request.user or
-                request.user.is_superuser
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_superuser
         )
