66f7424a712601fdc81af370f1791e22785e57b5
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-10-21 00:17:42 +0600

    воркфло_рецепты

--- backend/run.sh
+++ backend/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py add_data
 python manage.py collectstatic --no-input
+python manage.py add_data
 gunicorn backend.wsgi:application --bind 0:8000
 
 
