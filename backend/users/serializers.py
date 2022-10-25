5557083b32d059a6fdd2fc5adc3b011d30503382
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-10-25 20:57:04 +0600

    Подписка ошибка 500, правлю

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -25,7 +25,7 @@ class CurrentUserSerializer(UserSerializer):
         request = self.context.get("request")
         if obj == request.user or request.user.is_anonymous:
             return False
-        return Follow.objects.filter(user=request.user, author=obj.id).exists()
+        return Follow.objects.filter(user=request.user, author=obj).exists()
 
 
 class SupportRecipesSerializer(serializers.ModelSerializer):
@@ -38,7 +38,7 @@ class SupportRecipesSerializer(serializers.ModelSerializer):
 
 class SubscribeListSerializer(serializers.ModelSerializer):
     """Сериализатор Избранного"""
-    # is_subscribed = serializers.SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 
@@ -54,18 +54,18 @@ class SubscribeListSerializer(serializers.ModelSerializer):
             "recipes",
             "recipes_count"
         )
-        read_only_fields = ("recipes", "recipes_count")
+        read_only_fields = ("is_subscribed", "recipes", "recipes_count")
 
     def __check_user_authorized(self, obj):
         request = self.context.get('request')
         return request and request.user.is_authenticated
 
-    # def get_is_subscribed(self, obj):
-    #     request = self.context.get('request')
-    #     if self.__check_user_authorized(obj) is True:
-    #         return Follow.objects.filter(
-    #             user=request.user, author=obj).exists()
-    #     return False
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if self.__check_user_authorized(obj) is True:
+            return Follow.objects.filter(
+                user=request.user, author=obj).exists()
+        return False
 
     def get_recipes(self, obj):
         recipes = obj.recipes.all()
